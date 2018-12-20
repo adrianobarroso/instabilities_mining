2b8441df522794f8c2c37a6023fdcff607b9d417
Adriano Barroso <wiermann@broou.com>
2018-12-20 02:46:27 -0200

    First instance method on python

--- test.py
+++ test.py
@@ -3,11 +3,17 @@ import xray
 class HycomDataSet:
 
     # Initializer / Instance Attributes
-    def __init__(self, x_dataset, y_dataset):
-        self.x_dataset = x_dataset
-        self.y_dataset = y_dataset
+    def __init__(self, xdataset, ydataset):
+        self.xdataset = xdataset
+        self.ydataset = ydataset
 
-    # def get_
+    def print_xdataset(self):
+        print(self.xdataset)
+  
 
 x = xray.open_dataset('http://tds.hycom.org/thredds/dodsC/GLBa0.08/expt_90.6/2008/uvel', decode_times=False)
 y = xray.open_dataset('http://tds.hycom.org/thredds/dodsC/GLBa0.08/expt_90.6/2008/vvel', decode_times=False)
+
+print(HycomDataSet(x, y).print_xdataset())
+  
+
