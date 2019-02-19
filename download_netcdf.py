1e3b3d7c4c2c92e1f32f4d86cd514c2cf3a8993d
Adriano Barroso <wiermann@broou.com>
2019-02-18 23:18:51 -0300

    Add dataset.py but could become a yml file

--- download_netcdf.py
+++ download_netcdf.py
@@ -2,79 +2,20 @@ import os
 from tool_scripts import *
 import yaml
 from hycom_dataset import *
+from datasets import *
 
 if __name__ == "__main__":
     os.system('mkdir -p netcdf_files')
 
-    dataset_url_2013a = '%s/%s/%s/%svel?var=%s&north=-18&west=-55&east=-38&south=-36&disableProjSubset=on&horizStride=1&time_start=%s&time_end=%s&timeStride=1&vertCoord=0&addLatLon=true&accept=netcdf' % (
-        'http://ncss.hycom.org/thredds/ncss/GLBa0.08',
-        'expt_90.9',
-        '2013',
-        '__var__',
-        '__var__',
-        '2013-01-01T00:00:00Z',
-        '2013-08-20T00:00:00Z'
-    )
-
-    # import pdb; pdb.set_trace();
-    dataset_url_2013b = '%s/%s/%s/%svel?var=%s&north=-18&west=-55&east=-38&south=-36&disableProjSubset=on&horizStride=1&time_start=%s&time_end=%s&timeStride=1&vertCoord=0&addLatLon=true&accept=netcdf' % (
-        'http://ncss.hycom.org/thredds/ncss/GLBa0.08',
-        'expt_91.0',
-        '2013b',
-        '__var__',
-        '__var__',
-        '2013-08-21T00:00:00Z',
-        '2013-12-31T00:00:00Z'
-    )
-
-    dataset_url_2014a = '%s/%s/%s/%svel?var=%s&north=-18&west=-55&east=-38&south=-36&disableProjSubset=on&horizStride=1&time_start=%s&time_end=%s&timeStride=1&vertCoord=0&addLatLon=true&accept=netcdf' % (
-        'http://ncss.hycom.org/thredds/ncss/GLBa0.08',
-        'expt_91.0',
-        '2014',
-        '__var__',
-        '__var__',
-        '2014-01-01T00:00:00Z',
-        '2014-04-04T00:00:00Z'
-    )
-
-    dataset_url_2014b = '%s/%s/%s/%svel?var=%s&north=-18&west=-55&east=-38&south=-36&disableProjSubset=on&horizStride=1&time_start=%s&time_end=%s&timeStride=1&vertCoord=0&addLatLon=true&accept=netcdf' % (
-        'http://ncss.hycom.org/thredds/ncss/GLBa0.08',
-        'expt_91.1',
-        '2014',
-        '__var__',
-        '__var__',
-        '2014-04-05T00:00:00Z',
-        '2014-12-31T00:00:00Z'
-    )
-
-    dataset_url_2015 = '%s/%s/%s/%svel?var=%s&north=-18&west=-55&east=-38&south=-36&disableProjSubset=on&horizStride=1&time_start=%s&time_end=%s&timeStride=1&vertCoord=0&addLatLon=true&accept=netcdf' % (
-        'http://ncss.hycom.org/thredds/ncss/GLBa0.08',
-        'expt_91.1',
-        '2015',
-        '__var__',
-        '__var__',
-        '2015-01-01T00:00:00Z',
-        '2015-12-31T00:00:00Z'
-    )
-
-    dataset_url_2016 = '%s/%s/%s/%svel?var=%s&north=-18&west=-55&east=-38&south=-36&disableProjSubset=on&horizStride=1&time_start=%s&time_end=%s&timeStride=1&vertCoord=0&addLatLon=true&accept=netcdf' % (
-        'http://ncss.hycom.org/thredds/ncss/GLBa0.08',
-        'expt_91.2',
-        '2016',
-        '__var__',
-        '__var__',
-        '2016-04-18T00:00:00Z',
-        '2016-12-31T00:00:00Z'
-    )
-
     dataset_to_process = {
-        'dataset_url_2013a': dataset_url_2013a,
-        'dataset_url_2013b': dataset_url_2013b,
-        'dataset_url_2014a': dataset_url_2014a,
-        'dataset_url_2014b': dataset_url_2014b,
-        'dataset_url_2015': dataset_url_2015,
-        'dataset_url_2016': dataset_url_2016
+        'dataset_url_2013a': DATASET_URL_2013a,
+        'dataset_url_2013b': DATASET_URL_2013b,
+        'dataset_url_2014a': DATASET_URL_2014a,
+        'dataset_url_2014b': DATASET_URL_2014b,
+        'dataset_url_2015': DATASET_URL_2015,
+        'dataset_url_2016': DATASET_URL_2016
     }
+    
     for dataset in dataset_to_process:
         splitted_url = dataset_to_process[dataset].split("__var__")
         dimensions = ['u', 'v']
