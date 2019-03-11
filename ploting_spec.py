b6354bb1e85319000e146dc40c2cd11d61426692
Adriano Barroso <wiermann@broou.com>
2019-03-10 23:37:41 -0300

    Include new station (vortex) | Add new plot section on energy analisys

--- ploting_spec.py
+++ ploting_spec.py
@@ -25,22 +25,21 @@ os.system('mkdir -p %s' % dir_video)
 selef = 10**(ef.ef.sel(longitude=lon, latitude=lat, method='nearest'))
 selef_nocur = 10**(ef_nocur.ef.sel(longitude=lon, latitude=lat, method='nearest'))
 
-# import pdb; pdb.set_trace()
+dist = 50
 
-for dt in range(50, selef_nocur.time.shape[0]):
-    # dt = 400
+for dt in range(dist, selef_nocur.time.shape[0]):
     print 'Ploting ef for dt %s \n Lon %s | Lat %s' % (dt, lon, lat)
 
     date = pd.to_datetime(selef.time[dt].values).strftime('%Y-%m-%d %H:%M')
 
-    plt.title('Longitude=%s ; Latitude=%s \nEspectro de energia para o tempo %s' % (lon, lat, date))
+    plt.title('Longitude=%s ; Latitude=%s \nEspectro de energia por frequencia para o tempo %s' % (lon, lat, date))
     plt.plot(selef.f, selef[dt], '-k', label='ef com corrente')
     plt.plot(selef_nocur.f, selef_nocur[dt], '-.g', label='ef sem corrente')
     if sys.argv[3] == 'log':
         plt.xscale('log')
         plt.yscale('log')
-        plt.xlim(0.02,1.1)
-        plt.ylim(0.0002, 11.0)
+        plt.xlim(0.03, 1.0)
+        plt.ylim(0.00002, 30.0)
         figname = '%s/log_%03d_spec.png' % (dir_video, dt)
     else:
         plt.ylim(0, 16)
@@ -60,9 +59,8 @@ else:
     make_video = "ffmpeg -y -r 18 -pattern_type glob -i '%s/*.png' -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p results/analysis/ef/videos/ef_%s_lon_lat_%s_%s.mp4" % (dir_video, year, lon, lat)
 os.system(make_video)
 
-# raise
-dist = 50
-
+# New plot
+# 
 plt.title('Longitude=%s ; Latitude=%s \nEspectro de energia media no tempo' % (lon, lat))
 # plt.plot(selef.f, selef[200], '-k', label='ef com corrente')
 # plt.plot(selef_nocur.f, selef_nocur[200], '-.g', label='ef sem corrente')
@@ -71,9 +69,8 @@ plt.plot(selef_nocur.f, selef_nocur[50::].mean('time'), '-.g', label='ef sem cor
 if sys.argv[3] == 'log':
     plt.xscale('log')
     plt.yscale('log')
-    plt.xlim(0.02,1.1)
-    # plt.ylim(0.000002,1.3)
-    plt.ylim(0.0002, 11.0)
+    plt.xlim(0.04, 1.0)
+    plt.ylim(0.0002, 21.0)
     figname = '%s/log_mean_ef_%s_lon_lat_%s_%s.png' % (dir_analysis, year, lon, lat)
 else:
     plt.xlim(0.02, 0.8)
